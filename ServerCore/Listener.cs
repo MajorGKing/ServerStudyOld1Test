4138171a144535e7d7359e03e04c8af09d7b7765
MajorGKing <majorgking@gmail.com>
2024-11-30 22:49:03 +0900

    서버 스터디 Session 작업 중

---
+++ ServerCore/Listener.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace ServerCore
+{
+    class Listener
+    {
+        Socket _listenSocket;
+        Func<Session> _sessionFactory;
+
+        public void Init(IPEndPoint endPoint, Func<Session> sessionFactory)
+        {
+            _listenSocket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            _sessionFactory += sessionFactory;
+
+            _listenSocket.Bind(endPoint);
+
+            _listenSocket.Listen(10);
+
+            // for(int i = 0; i < 10; i++)
+            // {
+            SocketAsyncEventArgs args = new SocketAsyncEventArgs();
+            args.Completed += new EventHandler<SocketAsyncEventArgs>(OnAcceptCompleted);
+            RegisterAccept(args);
+            // }
+        }
+
+        void RegisterAccept(SocketAsyncEventArgs args)
+        {
+            args.AcceptSocket = null;
+
+            bool pending = _listenSocket.AcceptAsync(args);
+            if(pending == false)
+                OnAcceptCompleted(null, args);
+        }
+
+        void OnAcceptCompleted(object sender, SocketAsyncEventArgs args)
+        {
+            if(args.SocketError == SocketError.Success)
+            {
+                Session session = _sessionFactory.Invoke();
+                session.Start(args.AcceptSocket);
+                session.OnConnected(args.AcceptSocket.RemoteEndPoint);
+            }
+            else
+                System.Console.WriteLine(args.SocketError.ToString());
+
+            RegisterAccept(args);
+        }
+    }
+}
---
+++ ServerCore/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using ServerCore;
+
+namespace ServerCore
+{
+    class GameSession : Session
+    {
+        public override void OnConnected(EndPoint endPoint)
+        {
+            System.Console.WriteLine($"OnConnected : {endPoint}");
+
+            byte[] sendBuff = Encoding.UTF8.GetBytes("Welcome to MMORPG server !");
+            Send(sendBuff);
+
+            Thread.Sleep(10000);
+
+            Disconnect();
+        }
+
+        public override void OnDisconnected(EndPoint endPoint)
+        {
+            System.Console.WriteLine($"OnDisconnected : {endPoint}");   
+        }
+
+        public override void OnRecv(ArraySegment<byte> buffer)
+        {
+            string recvData = Encoding.UTF8.GetString(buffer.Array, buffer.Offset, buffer.Count);
+            System.Console.WriteLine($"[From Client] {recvData}");
+        }
+
+        public override void OnSend(int numOfBytes)
+        {
+            System.Console.WriteLine($"Transferred bytes: {numOfBytes}");   
+        }
+    }
+    class Program
+    {
+        static Listener _listener = new Listener();
+
+        static void Main(string[] args)
+        {
+            string host = Dns.GetHostName();
+            IPHostEntry ipHost = Dns.GetHostEntry(host);
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
+
+            _listener.Init(endPoint, () => {return new GameSession();});
+            System.Console.WriteLine("Listening...");
+
+            while (true)
+            {
+                ;
+            }
+        }
+    }
+}
