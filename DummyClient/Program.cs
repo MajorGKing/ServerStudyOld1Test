64643072f01897ea74c7aea4f541c640ff623466
MajorGKing <majorgking@gmail.com>
2024-12-02 20:59:21 +0900

    Server와 ServerCore 분리 Connector추가

--- DummyClient/Program.cs
+++ DummyClient/Program.cs
@@ -2,9 +2,39 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using ServerCore;
 
-namespace MyCSharpApp
+namespace DummyClient
 {
+    class GameSession : Session
+    {
+        public override void OnConnected(EndPoint endPoint)
+        {
+            System.Console.WriteLine($"OnConnected : {endPoint}");
+
+            for (int i = 0; i < 5; i++)
+            {
+                byte[] sendBuff = Encoding.UTF8.GetBytes($"Hello World! {i}");
+                Send(sendBuff);
+            }
+        }
+
+        public override void OnDisconnected(EndPoint endPoint)
+        {
+            System.Console.WriteLine($"OnDisconnected : {endPoint}");
+        }
+
+        public override void OnRecv(ArraySegment<byte> buffer)
+        {
+            string recvData = Encoding.UTF8.GetString(buffer.Array, buffer.Offset, buffer.Count);
+            System.Console.WriteLine($"[From Server] {recvData}");
+        }
+
+        public override void OnSend(int numOfBytes)
+        {
+            System.Console.WriteLine($"Transferred bytes: {numOfBytes}");
+        }
+    }
     class Program
     {
         static void Main(string[] args)
@@ -14,31 +44,14 @@ namespace MyCSharpApp
             IPAddress ipAddr = ipHost.AddressList[0];
             IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
 
+            Connector connector= new Connector();
+            connector.Connect(endPoint, () => {return new GameSession();});
+
             while (true)
             {
-
-                Socket socket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-
                 try
                 {
-                    socket.Connect(endPoint);
-                    System.Console.WriteLine($"Connected To {socket.RemoteEndPoint.ToString()}");
-
-
-                    for (int i = 0; i < 5; i++)
-                    {
-                        byte[] sendBuff = Encoding.UTF8.GetBytes($"Hello World! {i}");
-                        int sendBytes = socket.Send(sendBuff);
-                    }
-
-
-                    byte[] recvBuff = new byte[1024];
-                    int recvBytes = socket.Receive(recvBuff);
-                    string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes);
-                    System.Console.WriteLine($"[From Server] {recvData}");
-
-                    socket.Shutdown(SocketShutdown.Both);
-                    socket.Close();
+             
                 }
                 catch (Exception e)
                 {
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Net;
-using System.Net.Sockets;
+﻿using System.Net;
 using System.Text;
 using ServerCore;
 
-namespace ServerCore
+namespace Server
 {
     class GameSession : Session
     {
