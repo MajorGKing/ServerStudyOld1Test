aa30f7b1b064859dcbc9edb0ccbf5bb59a0320c0
MajorGKing <majorgking@gmail.com>
2024-12-04 20:24:08 +0900

    바이트 처리 및 리스트의 리스트 처리 추가

--- Tools/PacketGenerator/PacketFormat.cs
+++ Tools/PacketGenerator/PacketFormat.cs
@@ -5,8 +5,33 @@ using System.Threading.Tasks;
 
 namespace PacketGenerator
 {
+    // {0} 패킷 이름/번호 목록
+    // {1} 패킷 목록
     public class PacketFormat
     {
+        public static string fileFormat = 
+@"using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+using ServerCore;
+
+public enum PacketID
+{{
+    {0}
+}}
+
+{1}
+";
+
+        // {0} 패킷 이름
+        // {1} 패킷 번호
+        public static string packetEnumFormat =
+@"{0} = {1},";
+
         // {0} 패킷 이름
         // {1} 맴버 변수들
         // {2} 맴버 변수 Read
@@ -65,7 +90,7 @@ class {0}
 
         public static string memberListFormat =
 @"
-public struct {0}
+public class {0}
 {{
     {2}
 
@@ -92,6 +117,12 @@ public List<{0}> {1}s = new List<{0}>();";
 count += sizeof({2});";
 
         // {0} 변수 이름
+        // {1} 변수 형식
+        public static string readByteFormat = 
+@"this.{0} = ({1})segment.Array[segment.Offset + count];
+count += sizeof({1});";
+
+        // {0} 변수 이름
         public static string readStringFormat =
 @"ushort {0}Len = BitConverter.ToUInt16(s.Slice(count, s.Length - count));
 count += sizeof(ushort);
@@ -115,6 +146,12 @@ for(int i = 0; i < {1}Len; i++)
         // {1} 변수 형식
         public static string writeFormat =
 @"success &= BitConverter.TryWriteBytes(s.Slice(count, s.Length - count), this.{0});
+count += sizeof({1});";
+
+        // {0} 변수 이름
+        // {1} 변수 형식
+        public static string writeByteFormat =
+@"segment.Array[segment.Offset + count] = (byte)this.{0};
 count += sizeof({1});";
 
         // {0} 변수 이름
